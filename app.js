5d5821d36b6f2736abddef3ab0cf1fd56bef9712
Renzo <renzopcabaya@email.com>
2026-09-21 16:05:24 +0800

    Add game start detection

---
+++ app.js
@@ -0,0 +1,8 @@
+const startButton = document.querySelector(".start-btn");
+const gameArea = document.querySelector(".game-area");
+const reactionTime = document.querySelector(".reaction-time");
+const bestScore = document.querySelector(".best-score");
+
+startButton.addEventListener("click", () => {
+  console.log("Game started!");
+});
