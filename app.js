4772d84ac48c41c6488d534aad06afaba4a2f531
Renzo <renzopcabaya@email.com>
2026-09-21 17:19:58 +0800

    Add reaction time calculation

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const gameArea = document.querySelector(".game-area");
 const reactionTime = document.querySelector(".reaction-time");
 const bestScore = document.querySelector(".best-score");
 const target = document.querySelector(".target");
+let startTime;
 
 startButton.addEventListener("click", () => {
   console.log("Game started!");
 
   target.style.display = "none";
 
+  const delay = Math.random() * 3000;
+
   setTimeout(() => {
     target.style.display = "block";
-  }, 2000);
+    startTime = Date.now();
+  }, delay);
+});
+
+target.addEventListener("click", () => {
+  target.style.display = "none";
+
+  const endTime = Date.now();
+
+  const reaction = endTime - startTime;
+  reactionTime.textContent = reaction;
 });
